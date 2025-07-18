559122996df2fd71bdbbdc7305660c56e9d4b8c1
Rigellion-tech <mete@277home.com>
2025-07-18 18:26:29 -0400

    Update auth_email.py
    cross origin added.

--- auth_email.py
+++ auth_email.py
@@ -1,14 +1,14 @@
-# auth_email.py
-
 import smtplib
 from email.mime.text import MIMEText
 import os
+from flask_cors import cross_origin
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
 EMAIL_USER = os.getenv("EMAIL_USER")             # e.g. daydreamforge@gmail.com
 EMAIL_PASS = os.getenv("EMAIL_APP_PASSWORD")     # your 16-char Gmail app password
 
+@cross_origin(supports_credentials=True)
 def send_login_code(to_email: str, code: str):
     body = f"Your DayDream Forge login code is: {code}"
 
